638190e7cfeedd62a5a346c8b9696a3b11863e69
Richard Tingstad <richard.tingstad@gmail.com>
2019-02-17 11:17:57 +0100

    Add dist/timetable.png

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: __dirname + '/dist',
     publicPath: '/',
     filename: 'bundle.js'
   },
